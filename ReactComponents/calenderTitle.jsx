3704504ed302e844ca105ef84b10bdbdfe31bf84
Alvinw9 <33185325+Alvinw9@users.noreply.github.com>
2019-11-09 18:24:41 -0800

    Components for calender.html

---
+++ ReactComponents/calenderTitle.jsx
@@ -0,0 +1,11 @@
+var NewComponent = React.createClass({
+  render: function() {
+    return (
+
+      <div class="wow fadeIn">
+      	<h4 className="h2 text-center mb-5">{this.props.name}</h4>
+      </div>
+    
+    );
+  }
+});
